f92a8ff777d26d097273a6a4761a0497ea20e321
Nicolas Seibel <nicknicolasbatista@outlook.com>
2020-06-15 20:24:24 -0300

    Chess layer e imprimindo o tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -12,6 +12,14 @@ public class Board {
 		pieces = new Piece[rows][columns];
 	}
 
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
 	public int getColumns() {
 		return columns;
 	}
@@ -28,4 +36,11 @@ public class Board {
 		this.pieces = pieces;
 	}
 	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 }
